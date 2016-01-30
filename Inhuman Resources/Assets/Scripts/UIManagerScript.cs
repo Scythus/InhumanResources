f09c3556011b537770f771f3a182a5a51424b8e8
kallous <charliehargood@gmail.com>
2016-01-30 15:13:07 +0000

    UI mostly done (but ugly) - can browse candidates, see job, and accept a candidate

--- Inhuman Resources/Assets/Scripts/UIManagerScript.cs
+++ Inhuman Resources/Assets/Scripts/UIManagerScript.cs
@@ -13,14 +13,34 @@ public class TestCV {
     }
 }
 
+public class TestJob {
+
+    public string title;
+    public string text;
+
+    public TestJob(string ti, string te) {
+        title = ti;
+        text = te;
+    }
+
+}
+
 public class UIManagerScript : MonoBehaviour {
 
     private int currentCV;
     private List<TestCV> cvList;
+    private TestJob job;
     private CanvasRenderer renderedCV;
+    private CanvasRenderer renderedJob;
+    private CanvasRenderer renderedContract;
+    private bool jobshown;
 
     public CanvasRenderer cvPrefab;
+    public CanvasRenderer jobPrefab;
+    public CanvasRenderer contractPrefab;
     public Transform canvasTrans;
+
+    public Button shownJobBtn; //Dirty! You should do this properly!
 
 	// Use this for initialization
 	void Start () {
@@ -29,10 +49,13 @@ public class UIManagerScript : MonoBehaviour {
         cvList.Add(new TestCV("Sam McSamual", "I'm sam and I really hate cats"));
         cvList.Add(new TestCV("Lucy Lucyyyyyy", "I'm Lucy and I could take or leave cats"));
         cvList.Add(new TestCV("Rachel von Rachel", "My names Rachel and I don't know what a cat is"));
-
+        
         currentCV = 0;
 
+        job = new TestJob("Junior Cultist (applied frogs department)", "We need a candidate with a passion for subservience and minimal initiative. Enjoyment of frogs optional");
+
         renderCV();
+        showJob();
     }
 	
 	// Update is called once per frame
@@ -60,13 +83,75 @@ public class UIManagerScript : MonoBehaviour {
         renderCV();
     }
 
+    public void btnShowJob(){
+        Debug.Log("JOB CLICKED");
+
+        if (jobshown)
+        {
+            hideJob();
+        }
+        else {
+            showJob();
+        }
+    }
+
+    public void btnAccept() {
+        Debug.Log("ACCEPT CLICKED");
+
+        acceptCurrentCandidate();
+    }
+
+    public void btnNextJob()
+    {
+        Debug.Log("NEXT JOB CLICKED");
+
+        startNextJob();
+    }
+
     public void renderCV() {
         renderedCV = Instantiate(cvPrefab);
         renderedCV.gameObject.transform.SetParent(canvasTrans, false);
         renderedCV.GetComponentInChildren<Text>().text = cvList[currentCV].name+" \n\n "+ cvList[currentCV].text;
+
+        Button accept = renderedCV.GetComponentInChildren<Button>();
+        accept.onClick.AddListener(() => btnAccept());
     }
 
     public void destroyCV() {
         Destroy(renderedCV.gameObject);
-    } 
+    }
+
+    public void showJob()
+    {
+        jobshown = true;
+        shownJobBtn.GetComponentInChildren<Text>().text = "Hide Job";
+
+        renderedJob = Instantiate(jobPrefab);
+        renderedJob.gameObject.transform.SetParent(canvasTrans, false);
+        renderedJob.GetComponentInChildren<Text>().text = job.title + " \n\n " + job.text;
+    }
+
+    public void hideJob()
+    {
+        jobshown = false;
+        shownJobBtn.GetComponentInChildren<Text>().text = "Show Job";
+        Destroy(renderedJob.gameObject);
+    }
+
+
+    public void acceptCurrentCandidate() {
+        renderedContract = Instantiate(contractPrefab);
+        renderedContract.gameObject.transform.SetParent(canvasTrans, false);
+        renderedContract.GetComponentInChildren<Text>().text = "Contract for "+job.title + " \n\n " + "New Hire: "+cvList[currentCV].name+"\n\n Suitability for Job: F- \n Review: New hire is rubbish with frogs";
+
+        Button accept = renderedContract.GetComponentInChildren<Button>();
+        accept.onClick.AddListener(() => btnNextJob());
+
+    }
+
+    public void startNextJob() {
+
+    }
+
+    
 }
